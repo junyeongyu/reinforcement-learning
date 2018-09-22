bec08395432c2ccc273d7ac28ad86c0e74a42c9a
junyeongbluefin <=>
2018-09-22 12:13:49 -0400

    break out

--- src/gym_open_ai_breakout.py
+++ src/gym_open_ai_breakout.py
@@ -203,9 +203,15 @@ ACTION_MEANING = {
     17 : "DOWNLEFTFIRE",
 }
 
-env = AtariEnv(game='breakout')
+#env = AtariEnv(game='breakout')
+
+
+# Maximize your score in the Atari 2600 game Breakout.
+# In this environment, the observation is an RGB image of the screen, which is an array of shape (210, 160, 3) Each action is repeatedly performed for a duration of kk frames, where kk is uniformly sampled from \{2, 3, 4\}{2,3,4}.
+env = gym.make('Breakout-v0')
 env.reset()
 for _ in range(1000):
     env.render()
     env.step(env.action_space.sample()) # take a random action
 
+env.close()
