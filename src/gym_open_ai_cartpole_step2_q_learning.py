91d60d54b249835d1215c762d0fc08b7dae4742b
junyeongyu <Junyeong.Yu@MyGeorgian.ca>
2018-11-18 21:06:30 -0500

    Add deep q learning

--- src/gym_open_ai_cartpole_step2_q_learning.py
+++ src/gym_open_ai_cartpole_step2_q_learning.py
@@ -4,6 +4,19 @@
 # http://pages.cs.wisc.edu/~finton/qcontroller.html
 # https://gist.github.com/n1try/af0b8476ae4106ec098fea1dfe57f578
 
+# https://en.wikipedia.org/wiki/Q-learning
+# http://mnemstudio.org/path-finding-q-learning-tutorial.htm
+# Q(state, action) = R(state, action) + Gamma * Max[Q(next state, all actions)]
+# Q(state, action) = R(state, action) + Gamma * Max[Q(next state, all actions)]
+# Q(1, 5) = R(1, 5) + 0.8 * Max[Q(1, 2), Q(1, 5)] = 0 + 0.8 * Max(0, 100) = 80
+# Q(1, 5) = R(1, 5) + 0.8 * Max[Q(5, 1), Q(5, 4), Q(5, 5)] = 100 + 0.8 * 0 = 100
+
+# https://towardsdatascience.com/cartpole-introduction-to-reinforcement-learning-ed0eb5b58288
+# https://github.com/gsurma/cartpole/blob/master/cartpole.py
+
+## EASIST ONE
+# https://medium.freecodecamp.org/an-introduction-to-q-learning-reinforcement-learning-14ac0b4493cc
+
 
 import gym
 import numpy
